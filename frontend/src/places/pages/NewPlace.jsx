38a77d5476006adf802ae2f2ddc5b5f5a2ff03ab
KubaGlegola <kubaglegola@gmail.com>
2023-09-05 12:19:06 +0200

    finished add place form

--- frontend/src/places/pages/NewPlace.jsx
+++ frontend/src/places/pages/NewPlace.jsx
@@ -1,7 +1,103 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
+
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import "./NewPlace.css";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { isValid: action.isValid, value: action.value },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 
 const NewPlace = () => {
-  return <div>NewPlace</div>;
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback((id, isValid, value) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
+
+  const placeSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  return (
+    <form className="place-form" onSubmit={placeSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        errorText="Please enter a valid title."
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={inputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        errorText="Please enter a valid description (at least 5 char)."
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        type="text"
+        label="Address"
+        errorText="Please enter a valid address."
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
+    </form>
+  );
 };
 
 export default NewPlace;
--- frontend/src/shared/components/UIElements/Map.jsx
+++ frontend/src/shared/components/UIElements/Map.jsx
@@ -1,30 +1,30 @@
-import React, { useRef, useEffect } from "react";
-import { default as MapOL } from "ol/Map.js";
-import OSM from "ol/source/OSM.js";
-import TileLayer from "ol/layer/Tile.js";
-import View from "ol/View.js";
+import React, { useRef } from "react";
+// import { default as MapOL } from "ol/Map.js";
+// import OSM from "ol/source/OSM.js";
+// import TileLayer from "ol/layer/Tile.js";
+// import View from "ol/View.js";
 
 import "./Map.css";
 
 const Map = (props) => {
   const mapRef = useRef();
 
-  const { center, zoom } = props;
+  // const { center, zoom } = props;
 
-  useEffect(() => {
-    new MapOL({
-      target: mapRef.current.id,
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      view: new View({
-        center: [center.lng, center.lat],
-        zoom: zoom,
-      }),
-    });
-  }, [center, zoom]);
+  // useEffect(() => {
+  //   new MapOL({
+  //     target: mapRef.current.id,
+  //     layers: [
+  //       new TileLayer({
+  //         source: new OSM(),
+  //       }),
+  //     ],
+  //     view: new View({
+  //       center: [center.lng, center.lat],
+  //       zoom: zoom,
+  //     }),
+  //   });
+  // }, [center, zoom]);
 
   return (
     <div
